66e188ff7bfbdf1e7b5f8b9fb2da1ec4511d8750
Luke Woodhead <Luke.Woodhead@smoothstack.com>
2021-05-20 12:32:50 -0600

    Added print statements

--- DSS/src/dss/first/print.java
+++ DSS/src/dss/first/print.java
@@ -1,5 +1,18 @@
 package dss.first;
 
 public class print {
-
+	public static void main(String args[]) {
+		System.out.println("1)");
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println(".........");
+		System.out.println("2)");
+		System.out.println(".........\n****\n***\n**\n*");
+		System.out.println("3)");
+		System.out.println("     *\n    ***\n   *****\n  *******\n...........");
+		System.out.println("4)");
+		System.out.println("...........\n  *******\n   *****\n    ***\n     *");
+	}
 }
