f5c910212c63f30041dbfbbb9bfd07ef6dc43a98
Luke Woodhead <Luke.Woodhead@smoothstack.com>
2021-06-19 23:49:48 -0600

    s

--- DSS/src/com/ss/march/lib/LibraryDAO.java
+++ DSS/src/com/ss/march/lib/LibraryDAO.java
@@ -10,7 +10,7 @@ public interface LibraryDAO<T> {
 	static final String driver = "com.mysql.cj.jdbc.Driver";
 	static final String url = "jdbc:mysql://localhost:3306/library";
 	static final String username = "root";
-	static final String password = "Zambia2010";
+	static final String password = "";
 	
 	List<T> getAll() throws ClassNotFoundException, SQLException;
 	
